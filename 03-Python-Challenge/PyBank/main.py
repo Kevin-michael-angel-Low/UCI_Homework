7772b3a37880bf1a90c83a2f60618a28ef14a174
Kevin Low <kevinmlowg@yahoo.com>
2020-09-28 15:33:07 -0700

    finalized pythyon hw

--- 03-Python-Challenge/PyBank/main.py
+++ 03-Python-Challenge/PyBank/main.py
@@ -1,26 +1,20 @@
-# Pybank main 
+# Pybank Main Code
 import os
 import csv
 
 # set variables
 monthNum = 0
-
 netTotal = 0
-
 greatestInc = 0
-
 greatestDec = 0
-
 avgChange = 0
 
 # csv open path:
 pyBankPath = os.path.join("Resources", "PyBank.csv")
 
-
 # open the csv file:
 with open(pyBankPath) as bankFile:
     bankReader = csv.reader(bankFile, delimiter=",")
-
     # skip the header
     next(bankFile)
 
@@ -30,13 +24,14 @@ with open(pyBankPath) as bankFile:
         if row[0] != "":
             monthNum = monthNum + 1
             netTotal = netTotal + int(row[1])
-
+            #find greatest increase and decrease of the csv
             if greatestInc < int(row[1]):
                 greatestInc = int(row[1])
                 gIncName = row[0]
             if greatestDec > int(row[1]):
                 greatestDec = int(row[1])
                 gDecName = row[0]
+    #calculate average change
     avgChange = netTotal / monthNum
     
 # formats the data for readability
@@ -51,7 +46,7 @@ outputString = (f"Financial Analysis \n -------------------\n"
 print(outputString)
 
 
-# Output analysis file path    
+# Find file path to anaylsis
 outputPath = os.path.join("analysis", "analysis.txt")
 
 # opens the output file and writes the output to text file
