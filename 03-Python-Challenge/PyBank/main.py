ab3eb1f1c525e9b26f516a328153420f3f95ddb7
Kevin Low <kevinmlowg@yahoo.com>
2020-09-27 17:59:15 -0700

    Completed Pt. 1 of python HW

---
+++ 03-Python-Challenge/PyBank/main.py
@@ -0,0 +1,60 @@
+# Pybank main 
+import os
+import csv
+
+# set variables
+monthNum = 0
+
+netTotal = 0
+
+greatestInc = 0
+
+greatestDec = 0
+
+avgChange = 0
+
+# csv open path:
+pyBankPath = os.path.join("Resources", "PyBank.csv")
+
+
+# open the csv file:
+with open(pyBankPath) as bankFile:
+    bankReader = csv.reader(bankFile, delimiter=",")
+
+    # skip the header
+    next(bankFile)
+
+# iterate through the csv
+    for row in bankReader:
+        # as long as the row is not empty
+        if row[0] != "":
+            monthNum = monthNum + 1
+            netTotal = netTotal + int(row[1])
+
+            if greatestInc < int(row[1]):
+                greatestInc = int(row[1])
+                gIncName = row[0]
+            if greatestDec > int(row[1]):
+                greatestDec = int(row[1])
+                gDecName = row[0]
+    avgChange = netTotal / monthNum
+    
+# formats the data for readability
+outputString = (f"Financial Analysis \n -------------------\n"
+                f"Total Months: {monthNum} \n"
+                f"Total: ${netTotal}\n"
+                f"Average Change: ${format(avgChange, '.2f')}\n"
+                f"Greatest Increase in Profits: {gIncName} (${greatestInc})\n"
+                f"Greatest Decrease in Profits: {gDecName} (${greatestDec})"
+            )
+#prints output string to the terminal
+print(outputString)
+
+
+# Output analysis file path    
+outputPath = os.path.join("analysis", "analysis.txt")
+
+# opens the output file and writes the output to text file
+with open(outputPath, 'w') as outputFile:
+    outputFile.write(outputString)
+
