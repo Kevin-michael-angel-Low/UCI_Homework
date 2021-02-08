3be864f9163ab9926f82d32ef3fdd08c4d5e29d8
Midgetman Low <kevinmlowg@yahoo.com>
2021-02-07 17:56:19 -0800

    fixed line 10 dataRow var

--- 13-Javascript-Challenge/UFO-level-1/static/js/app.js
+++ 13-Javascript-Challenge/UFO-level-1/static/js/app.js
@@ -7,7 +7,7 @@ var tbody = d3.select("tbody");
 function buildTable(data){
     tbody.html("");
 
-    data.forEach((data.Row) => {
+    data.forEach((dataRow) => {
         var row = tbody.append("tr");
 
         Object.values(dataRow).forEach((val) => {
@@ -17,3 +17,18 @@ function buildTable(data){
     });
 }
 
+function clicker(){
+
+    var date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    if (date){
+        filteredData = filteredData.filter(row => row.datetime === date);
+    }
+
+    buildTable(filteredData);
+}
+
+d3.selectAll("#filter-btn").on("click", clicker);
+
+buildTable(tableData);
