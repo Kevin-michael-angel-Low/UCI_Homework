79c7b47fe37f25784619e081163c59c95799e61e
Midgetman Low <kevinmlowg@yahoo.com>
2021-02-07 18:55:42 -0800

    some minor comment edits, added readme

---
+++ 13-Javascript-Challenge/README.md
@@ -0,0 +1,3 @@
+## Javascript Homework
+
+Used a combination of d3 in javascript along with a provided html code to filter a larger dataset, while also displaying it onto the page. 
--- 13-Javascript-Challenge/UFO-level-1/static/js/app.js
+++ 13-Javascript-Challenge/UFO-level-1/static/js/app.js
@@ -5,11 +5,15 @@ var tableData = data;
 var tbody = d3.select("tbody");
 
 function buildTable(data){
-    tbody.html("");
 
+    //clear table for a blank slate
+    tbody.html("");
+    
+    //append data rows to the end of table
     data.forEach((dataRow) => {
         var row = tbody.append("tr");
 
+        // set values
         Object.values(dataRow).forEach((val) => {
             let cell = row.append("td");
                 cell.text(val);
@@ -19,16 +23,21 @@ function buildTable(data){
 
 function clicker(){
 
+    // put filter value into a variable
     var date = d3.select("#datetime").property("value");
     let filteredData = tableData;
 
+    // only keep the rows where the "datetime" is equal to the filter
     if (date){
         filteredData = filteredData.filter(row => row.datetime === date);
     }
 
+    // build the table using previous function
     buildTable(filteredData);
 }
 
+// listener
 d3.selectAll("#filter-btn").on("click", clicker);
 
+// load table
 buildTable(tableData);
